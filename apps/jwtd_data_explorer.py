8fbf940d6292d853c0be2cc5893e41fec9bbc921
Tomoaki Nakamura <tyo_yo@icloud.com>
2020-12-14 04:53:45 +0900

    Update to reduce memory usage

--- apps/jwtd_data_explorer.py
+++ apps/jwtd_data_explorer.py
@@ -2,6 +2,7 @@ import shutil
 import tarfile
 from pathlib import Path
 
+import dask
 import dask.dataframe as dd
 import pandas as pd
 import streamlit as st
@@ -13,7 +14,6 @@ def app():
     st.title("日本語Wikipedia入力誤りデータセット ビューワー")
     st.write("http://nlp.ist.i.kyoto-u.ac.jp/?日本語Wikipedia入力誤りデータセット")
 
-    @st.cache
     def download_data():
         if Path("data/jwtd").exists():
             return
@@ -22,54 +22,64 @@ def app():
             with tarfile.open(fileobj=f) as tar:
                 tar.extractall("data")
 
-    text = st.text("Donwloding dataset...")
     download_data()
-    text.text(f"Donwload Successed!")
 
-    # @st.cache
+    # ここでキャッシュすることで、df_trainなどがリロードしても同一のオブジェクトになり、
+    # df_trainを参照している関数のキャッシュがうまく動くようになる
+    @st.cache(show_spinner=False)
     def load_dataframe(path):
-        df = dd.read_json(path, lines=True, orient="records")
-        return df
+        return dd.read_json(path, blocksize="1MB")
 
     df_train = load_dataframe("data/jwtd/train.jsonl")
+    df_test = load_dataframe("data/jwtd/test.jsonl")
 
-    # st.write("### File tree")
-    # st.text(display_tree("data/jwtd", string_rep=True))
+    st.write("### File tree")
+    st.text(display_tree("data/jwtd", string_rep=True))
 
-    # st.sidebar.write("## Display settings")
-    # cols = st.sidebar.multiselect(
-    #     "Filter columns", list(df_train.columns), default=list(df_train.columns),
-    # )
-    # n_data = st.sidebar.selectbox(
-    #     "Max display rows: ", options=[1, 2, 3, 5, 10, 100], index=3
-    # )
-    # query = st.sidebar.text_input("query", value="")
-    # st.sidebar.info(
-    #     "query syntax is based on pandas.query, e.g. category=='kanji-conversion'"
-    # )
+    st.sidebar.write("## Display settings")
+    cols = st.sidebar.multiselect(
+        "Filter columns", list(df_train.columns), default=list(df_train.columns),
+    )
+    n_data = st.sidebar.selectbox(
+        "Max display rows: ", options=[1, 2, 3, 5, 10, 100], index=3
+    )
 
-    # st.write("### Train.jsonl")
-    # if query:
-    #     df_view = df_train.query(query)
-    # else:
-    #     df_view = df_train
-    # df_view = df_view[cols]
+    button_placeholder = st.sidebar.empty()
+    query = st.sidebar.text_input("query", value="")
+    st.sidebar.info(
+        "Query syntax is based on pandas.query, e.g. category=='kanji-conversion', post_text.str.contains('キャンパス')"
+    )
 
-    # if st.sidebar.button("Shuffle Table"):
-    #     df_view = df_view.sample(frac=1 / n_data)
-    # st.table(df_view.head(n=n_data))
+    st.write("## Browse Train Dataset")
 
-    # st.write("### Stats")
-    # st.write("Categories")
+    if query:
+        with st.spinner(
+            "Query need whole dataset access and slow down browsing. To speed up, remove all queries."
+        ):
+            df_view = df_train.query(query, engine="python").compute()
+    else:
+        df_view = df_train
+    df_view = df_view[cols]
 
-    # @st.cache
-    def count_categories():
-        return df_train.category.value_counts().compute()
+    if button_placeholder.button("Random Sampling"):
+        df_view = df_view.sample(frac=1 / n_data)
+    st.table(df_view.head(n_data))
 
-    st.write(count_categories())
+    st.write("## Stats")
 
-    # @st.cache
-    # def describe_df():
-    #     return df_train.describe().compute()
+    @st.cache
+    def calc_stats():
+        return dask.compute(
+            df_train.size,
+            df_test.size,
+            df_train.category.value_counts(),
+            df_train.describe(),
+        )
 
-    # st.write(describe_df())
+    n_train, n_test, category_stats, num_stats = calc_stats()
+
+    st.write(f"Train data size: {n_train}, Test data size: {n_test}")
+    st.write("Categories")
+
+    st.write(category_stats)
+    st.write(num_stats)
