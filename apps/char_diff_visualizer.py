21384efa5647431f10bc43507aadf1ebdedbd551
Tomoaki Nakamura <tyo_yo@icloud.com>
2020-12-15 18:19:52 +0900

    Install chardiff_html

--- apps/char_diff_visualizer.py
+++ apps/char_diff_visualizer.py
@@ -1,48 +1,15 @@
 import difflib
 
 import streamlit as st
+from chardiff_html import chardiff_html
 
 
 def app():
     st.write("### Input")
     str1 = st.text_area("Original Sentence", "This is orignal sentence.")
     str2 = st.text_area("New Sentence", "That is new sentence.")
-    res = visualize_chardiff(str1, str2)
+    res = chardiff_html(str1, str2)
     st.markdown("### Diffs")
     st.markdown(
         res, unsafe_allow_html=True,
     )
-
-
-def visualize_chardiff(
-    str1,
-    str2,
-    del_font_color="red",
-    del_background_color="mistyrose",
-    ins_font_color="green",
-    ins_background_color="#e0ffe5",
-    **kwargs,
-):
-    # State meaning:: ' ': equal, '+': insert, '-': delete
-    prev_state, state = " ", " "
-    output = ""
-    for diff in difflib.ndiff(str1, str2, **kwargs):
-        prev_state = state
-        state, _, char = diff
-
-        if state == prev_state:
-            output += char
-            continue
-
-        if prev_state != " ":
-            output += "</span>"
-
-        if state == "+":
-            output += f'<span style="color: {ins_font_color}; background-color: {ins_background_color}">'
-        elif state == "-":
-            output += f'<span style="color: {del_font_color}; background-color: {del_background_color}">'
-        output += char
-
-    if state != " ":
-        output += "</span>"
-    return output
