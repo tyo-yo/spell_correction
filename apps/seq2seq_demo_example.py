ed08c55245e565ff1a45b9b6d1c689af97c461df
Tomoaki Nakamura <tyo_yo@icloud.com>
2020-12-19 21:15:14 +0900

    Update seq2seq demo

--- apps/seq2seq_demo_example.py
+++ apps/seq2seq_demo_example.py
@@ -1,10 +1,3 @@
-import difflib
-import importlib
-
-import spacy
-
-# if importlib.util.find_spec("spell_correction") is None:
-import spell_correction
 import streamlit as st
 from allennlp.predictors import Predictor
 from chardiff_html import chardiff_html
@@ -20,7 +13,11 @@ def app():
     input_sentence = st.text_area("Input Sentence", "これが正されるのは、江戸時代に本居宣長の登場してからのことである。")
 
     archive_path = st.sidebar.selectbox(
-        label="Select Model", options=["experiments/jwtd_test/0000_cpu/model.tar.gz"]
+        label="Select Model",
+        options=[
+            "https://storage.googleapis.com/tyoyo/experiments/jwtd_test/0000_cpu/model.tar.gz"
+        ],
+        format_func=lambda path: "..." + path.replace("/model.tar.gz", "")[-27:],
     )
     predictor = load_model(archive_path)
     output_json = predictor.predict(input_sentence)
