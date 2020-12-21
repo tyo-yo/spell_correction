997790f2f85866c6f13742cb14f6341a9fa5c620
Tomoaki Nakamura <tyo_yo@icloud.com>
2020-12-22 00:32:01 +0900

    Fix __init__

--- spell_correction/__init__.py
+++ spell_correction/__init__.py
@@ -1,3 +1,4 @@
 from spell_correction.callbacks import LogToComet
 from spell_correction.metrics import LevenshteinDistance
 from spell_correction.predictor import JaSeq2SeqPredictor
+from spell_correction.tokenizer import MeCabTokenizer
