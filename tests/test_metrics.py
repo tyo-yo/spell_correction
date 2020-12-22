204732a870a35dd238f2ba2ef15b676972d21450
Tomoaki Nakamura <tyo_yo@icloud.com>
2020-12-22 18:19:05 +0900

    Update test

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -8,18 +8,21 @@ def test_levenshtein_distance():
 
     metric(pred, gold)
     val = metric.get_metric()
-    assert val == 2
+    assert val["LevenshteinDistance"] == 2
+    assert val["ExactMatch"] == 0
 
     pred, gold = ["ほge", "fgdx"], ["hoge", "fuga"]  # distance: 2, 3
     metric(pred, gold)
     val = metric.get_metric(reset=True)
-    assert val == (2 + 2 + 3) / 3
+    assert val["LevenshteinDistance"] == (2 + 2 + 3) / 3
+    assert val["ExactMatch"] == 0
 
     pred, gold = (
-        ["appl", "apple", "apple"],
+        ["ap", "apple", "apple"],
         ["apple", "apple", "apple"],
     )  # distance: 1, 0, 0
     metric(pred, gold)
     val = metric.get_metric()
-    assert val == (1 + 0 + 0) / 3
+    assert val["LevenshteinDistance"] == (3 + 0 + 0) / 3
+    assert val["ExactMatch"] == (2 + 0 + 0) / 3
 
