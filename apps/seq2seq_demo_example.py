c18a0dd7263d2ac0948178feb66c22d3184a25ac
Tomoaki Nakamura <tyo_yo@icloud.com>
2020-12-26 17:02:31 +0900

    Update app

--- apps/seq2seq_demo_example.py
+++ apps/seq2seq_demo_example.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+import pandas as pd
 import streamlit as st
 from allennlp.common.params import Params
 from allennlp.data.dataset_readers import DatasetReader
@@ -7,6 +8,7 @@ from allennlp.models import Model
 from allennlp.predictors import Predictor
 from allennlp_models.generation.dataset_readers import Seq2SeqDatasetReader
 from chardiff_html import chardiff_html
+from smart_open import open
 from spell_correction.predictor import JaSeq2SeqPredictor
 
 
@@ -33,12 +35,14 @@ def app():
     # )
     path = st.sidebar.text_input(
         "Model URL or Path",
-        value="https://storage.googleapis.com/tyoyo/experiments/jwtd_test/0000_cpu/model.tar.gz",
+        value="https://storage.googleapis.com/tyoyo/experiments/jwtd/0027_lstm/model.tar.gz",
     )
     predictor = load_predictor(path)
 
     st.write("### Input")
-    input_sentence = st.text_area("Input Sentence", "これが正されるのは、江戸時代に本居宣長の登場してからのことである。")
+    input_sentence = st.text_area(
+        "Input Sentence", "声優業の創世記から活動するベテランであり、アニメ、吹き替え、ナレーションと幅広く活動。"
+    )
 
     output_json = predictor.predict(input_sentence)
     output_sentence = "".join(output_json["predicted_tokens"])
@@ -46,11 +50,23 @@ def app():
     st.write("### Output")
     st.write(output_sentence)
 
-    st.markdown("### Diffs")
+    st.markdown("### Diffs between ")
     res = chardiff_html(input_sentence, output_sentence)
     st.markdown(
         res, unsafe_allow_html=True,
     )
+
+    st.markdown("### Validation Data Example")
+
+    @st.cache
+    def load_validation_dataset():
+        with open("gs://tyoyo/jwtd/v1.0/dev.tsv") as f:
+            df = pd.read_csv(f, sep="\t", header=None)
+        return df
+
+    df = load_validation_dataset()
+    df.columns = ["元データ", "正解"]
+    st.table(df.sample(n=5))
 
     st.markdown("### Detailed Ouput Json")
     st.write(output_json)
