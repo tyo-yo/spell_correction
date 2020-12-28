30acbe31358e0ab8661f62098229fc3e552db2a5
Tomoaki Nakamura <tyo_yo@icloud.com>
2020-12-29 04:15:13 +0900

    Save

--- spell_correction/t5_spell_corrector.py
+++ spell_correction/t5_spell_corrector.py
@@ -1,37 +1,32 @@
 from functools import lru_cache
-from typing import Any, Dict, List, Tuple
+from itertools import product
+from typing import Any, Callable, Dict, List, Tuple
 
 from pydantic import BaseModel
+from torch.nn.functional import log_softmax
 from transformers import MT5ForConditionalGeneration, T5Tokenizer
 
-from spell_correction.utils import BeginningLevenshtein, sigmoid
+from spell_correction.utils import BeginningLevenshtein
 
-
-def default_score_func(candidate):
-    return (
-        candidate["match_len"]
-        / (candidate["distance"] + 1)
-        * sigmoid(candidate["logit"])
-    )
-
-
-def default_filter_func(candidate):
-    return candidate["distance"] <= 2 and candidate["match_len"] >= 1
+OVEREDIT = "OVEREDIT"
+FINISHED = "FINISHED"
+PROCESSING = "PROCESSING"
 
 
 class Edit(BaseModel):
-    str_after: str
-    str_before: str
+    text_after: str
+    text_before: str
     distance: float
-    likelihood: float
+    log_likelihood: float
     likelihood_rank: int
 
 
 class Beam(BaseModel):
     masked_text: str
-    remaining_str: str
+    remaining_text: str
+    filled_text: str = ""
     total_distance: float = 0.0
-    total_logits: float = 0.0
+    total_log_likelihood: float = 0.0
     changes: List[Edit] = []
 
 
@@ -39,14 +34,14 @@ class T5SpellCorrector:
     def __init__(
         self,
         model_name="google/mt5-small",
-        max_edit_distance: int = 2,
-        substitution_cost_fn=lambda str1, str2: 1,
-        insertion_cost_fn=lambda _: 1,
-        deletion_cost_fn=lambda _: 1,
-        max_decoding_steps: int = 10,
+        max_total_distance: int = 2,
+        max_each_distance: int = 2,
+        substitution_cost_fn: Callable[[str, str], float] = lambda str1, str2: 1.0,
+        insertion_cost_fn: Callable[[str], float] = lambda _: 1.0,
+        deletion_cost_fn: Callable[[str], float] = lambda _: 1.0,
+        score_fn: Callable[[Edit], float] = lambda edit: edit.likelihood,
+        filter_fn: Callable[[Edit], bool] = lambda edit: True,
         max_rank: int = 50000,
-        score_func=default_score_func,
-        filter_func=default_filter_func,
         add_original_token_to_masked_text: bool = True,
     ):
         self.model = MT5ForConditionalGeneration.from_pretrained(model_name)
@@ -54,7 +49,8 @@ class T5SpellCorrector:
             p.requires_grad = False
 
         self.tokenizer = T5Tokenizer.from_pretrained(model_name)
-        self.max_edit_distance = max_edit_distance
+        self.max_each_distance = max_each_distance
+        self.max_total_distance = max_total_distance
 
         self.beginning_levenshtein = BeginningLevenshtein(
             substitution_cost_fn=substitution_cost_fn,
@@ -65,10 +61,9 @@ class T5SpellCorrector:
             maxsize=self.tokenizer.vocab_size ** 2
         )(self.beginning_levenshtein.__call__)
 
-        self.max_decoding_steps = max_decoding_steps
         self.max_rank = max_rank
-        self.score_func = score_func
-        self.filter_func = filter_func
+        self.score_fn = score_fn
+        self.filter_fn = filter_fn
         self.add_original_token_to_masked_text = add_original_token_to_masked_text
 
     def predict(self, text: str) -> Dict[str, Any]:
@@ -83,42 +78,39 @@ class T5SpellCorrector:
         # return masked_text.replace(" <extra_id_0>", ""), changes
 
     def beam_search(self, masked_text, original_token) -> List[Dict[str, Any]]:
-        beam_stack = [Beam(masked_text=masked_text, remaining_str=original_token)]
+        beam_stack = [Beam(masked_text=masked_text, remaining_text=original_token)]
         beam_finished = []
 
         while beam_stack:
             beam = beam_stack.pop()
-            possible_beams = self._process_beam(beam)
+            possible_beams = self.calc_possible_beams(beam)
             for possible_beam in possible_beams:
                 if self._is_finished_beam(possible_beam):
                     beam_finished.append(possible_beam)
                 else:
                     beam_stack.append(possible_beam)
 
-    def _process_beam(self, beam: Beam) -> List[Beam]:
-        raise NotImplementedError
-        remaining_str = original_token
-        changes = []
+    def _get_beam_status(self, beam: Beam) -> str:
+        if beam.total_distance > self.max_total_distance:
+            return OVEREDIT
+        elif len(beam.remaining_str) == 0:
+            return FINISHED
+        else:
+            return PROCESSING
 
-        for step in range(self.max_decoding_steps):
-            candidates = self.calc_next_token_candidates(masked_text, remaining_str)
-            selected = candidates[0]
-            changes.append(selected)
+    def _apply_edit_to_beam(self, edit: Edit, beam: Beam) -> Beam:
+        beam = beam.copy(deep=True)
+        beam.remaining_text = beam.remaining_text[len(edit.text_before) :]
+        beam.filled_text += edit.text_after
+        beam.total_distance += edit.distance
+        beam.total_log_likelihood += edit.log_likelihood
+        beam.changes.append(edit)
+        return beam
 
-            masked_text = masked_text.replace(
-                " <extra_id_0>", f"{selected['token']} <extra_id_0>"
-            )
-            remaining_str = remaining_str[selected["match_len"] :]
-            if remaining_str == "":
-                break
-
-    def _is_finished_beam(self, beam: Beam) -> List[Beam]:
-        return len(beam.remaining_str) == 0
-
-    def calc_next_token_candidates(self, masked_text, remaining_str):
+    def calc_possible_edits(self, beam: Beam) -> List[Edit]:
         batch = self.tokenizer.prepare_seq2seq_batch(
-            src_texts=[masked_text],
-            tgt_texts=["<pad> <extra_id_0>"],
+            src_texts=[beam.masked_text],
+            tgt_texts=["<pad> <extra_id_0>" + beam.filled_text],
             return_tensors="pt",
         )
 
@@ -134,32 +126,87 @@ class T5SpellCorrector:
         # batch size = 1 前提のコード
         top_tokens = batch_top_tokens[0]
         top_logits = batch_top_logits[0]
-        top_indices = batch_top_indices[0]
+        top_log_likelihoods = log_softmax(top_logits)
 
-        candidates = []
-        for rank, (token, logit, index) in enumerate(
-            zip(top_tokens, top_logits, top_indices)
+        possible_edits = []
+        for rank, (token, log_likelihood) in enumerate(
+            zip(top_tokens, top_log_likelihoods)
         ):
             if token == self.tokenizer.eos_token:
-                candidate = {
-                    "match_ref": remaining_str,
-                    "match_len": len(remaining_str),
-                    "distance": len(remaining_str),
-                    "rank": rank,
-                    "token": "",
-                    "logit": logit.tolist(),
-                    "index": index.tolist(),
-                }
+                possible_edit = Edit(
+                    text_after="",
+                    text_before=beam.remaining_text,
+                    distance=len(beam.remaining_text),
+                    log_likelihood=log_likelihood.tolist(),
+                    likelihood_rank=rank,
+                )
             else:
-                candidate = self.beginning_levenshtein(token, remaining_str)
-                candidate["rank"] = rank
-                candidate["token"] = token
-                candidate["logit"] = logit.tolist()
-                candidate["index"] = index.tolist()
-            candidates.append(candidate)
+                diff = self.beginning_levenshtein(token, beam.remaining_text)
+                possible_edit = Edit(
+                    text_after=token,
+                    text_before=diff["match_ref"],
+                    distance=diff["distance"],
+                    log_likelihood=log_likelihood.tolist(),
+                    likelihood_rank=rank,
+                )
+            possible_edits.append(possible_edit)
+
+            # 計算量を減らすために尤度が低いものは足切り
             if rank >= self.max_rank:
                 break
 
-        candidates = filter(self.filter_func, candidates)
-        candidates = sorted(candidates, key=self.score_func, reverse=True)
-        return candidates
+        possible_edits = filter(self.filter_fn, possible_edits)
+        possible_edits = sorted(possible_edits, key=self.score_fn, reverse=True)
+        return possible_edits
+
+    def take_beam_step(self, beam: Beam, len_original_token: int) -> List[Beam]:
+        possible_edits = self.calc_possible_edits(beam)
+        hopeful_edits = self.filter_hopeful_edits(
+            possible_edits, len_original_token, self.max_each_distance
+        )
+
+        possible_beams = [
+            self._apply_edit_to_beam(edit, beam) for edit in possible_edits
+        ]
+
+        return possible_beams
+
+    def filter_hopeful_edits(
+        self, sorted_edits: List[Edit], len_original_token: int,
+    ) -> List[Edit]:
+
+        hopeful_edits = []
+        # 尤度が高く、より一致する部分が多く、編集距離が小さいものがより良いEdit だが、それを選ぶのは難しい
+        # そこで、一致度合いと編集距離の各パターンにおいて尤度が最も高くなるEditを全てhopefulとする
+        # (match_len, distance) = (0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2) ... に対して
+        for match_len, distance in product(
+            range(len_original_token + 1), range(self.max_each_distance + 1)
+        ):
+            # 条件を満たすものをフィルタした中から、最も尤度が高いものを選択
+            # （尤度が高いものからソートされているので、next()で大丈夫）
+            hopeful_edit = next(
+                filter(
+                    lambda edit: len(edit.text_before) == match_len
+                    and edit.distance == distance,
+                    sorted_edits,
+                )
+            )
+            hopeful_edits.append(hopeful_edit)
+        return hopeful_edits
+
+    # def _process_beam(self, beam: Beam) -> List[Beam]:
+    # raise NotImplementedError
+    # remaining_text = original_token
+    # changes = []
+
+    # for step in range(self.max_decoding_steps):
+    #     candidates = self.calc_next_token_candidates(masked_text, remaining_text)
+    #     selected = candidates[0]
+    #     changes.append(selected)
+
+    #     masked_text = masked_text.replace(
+    #         " <extra_id_0>", f"{selected['token']} <extra_id_0>"
+    #     )
+    #     remaining_text = remaining_text[selected["match_len"] :]
+    #     if remaining_text == "":
+    #         break
